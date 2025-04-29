0ac1cbd40715b4e28cc20b73d9cb3807623df617
Lucas <lucasprepara12@gmail.com>
2025-04-29 10:17:15 -0300

    First commit

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Raleway } from "next/font/google";
+import "./globals.css";
+
+const raleway = Raleway({
+  variable: "--font-raleway",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "PWA Cache Project",
+  description: "A PWA Cache Project",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${raleway.className} h-dvh vsc-initialized`}>
+        {children}
+      </body>
+    </html>
+  );
+}
